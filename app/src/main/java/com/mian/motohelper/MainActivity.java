fa8247ba2152bf9c511e5017e4f88e995869bba0
Wilson Huang <hjn201@gmail.com>
2015-12-16 10:36:06 +0800

    2015/12/16 10:35

--- app/src/main/java/com/mian/motohelper/MainActivity.java
+++ app/src/main/java/com/mian/motohelper/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 }
