15e619a870970407627a6f082735fefbd8dbdd33
Wilson Huang <hjn201@gmail.com>
2015-12-23 20:14:00 +0800

    2015/12/23 20:13

--- googlemaps_demo_camera/src/main/java/com/mian/googlemaps_demo_camera/Helper.java
+++ googlemaps_demo_camera/src/main/java/com/mian/googlemaps_demo_camera/Helper.java
@@ -2,11 +2,140 @@ package com.mian.googlemaps_demo_camera;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 /**
  * Created by WilsonHuang on 2015/12/22.
  */
 public class Helper {
+    public static String DOMAIN = "http://maps.googleapis.com/maps/api/geocode/json";
+
+
     public static String getAddressByLatLng(LatLng latLng) {
-        return null;
+        String address = "";
+        Helper.LatLngToAddress ga = new Helper.LatLngToAddress(latLng);
+        FutureTask<String> futureTask = new FutureTask<String>(ga);
+        new Thread(futureTask).start();
+        try {
+            address = futureTask.get();
+        } catch (Exception ex) {
+
+        }
+        return address;
+    }
+
+
+    public static LatLng getLatLngByAddress(String address) {
+        LatLng latLng = null;
+        Helper.AddressToLatLng ga = new Helper.AddressToLatLng(address);
+        FutureTask<LatLng> futureTask = new FutureTask<LatLng>(ga);
+        new Thread(futureTask).start();
+        try {
+            latLng = futureTask.get();
+        } catch (Exception ex) {
+
+        }
+        return latLng;
+    }
+
+    private static class HttpUtil {
+        public static String get(String urlString) throws Exception {
+            InputStream inputStream = null;
+            Reader reader = null;
+            StringBuilder str = new StringBuilder();
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("User-agent", "IE/6.0");
+            inputStream = urlConnection.getInputStream();
+            reader = new InputStreamReader(inputStream, "UTF-8");
+            char[] buffer = new char[1];
+            while (reader.read(buffer) != -1) {
+                str.append(new String(buffer));
+            }
+            return str.toString();
+        }
+    }
+
+    private static class LatLngToAddress implements Callable<String> {
+        private String queryURLString = DOMAIN + "?latlng=%s,%s&sensor=true&language=zh_tw";
+        private String address = null;
+
+        private LatLng latLng;
+
+        LatLngToAddress(LatLng latLng) {
+            this.latLng = latLng;
+        }
+
+        @Override
+        public String call() throws Exception {
+            address = getAddressByLocation();
+            return address;
+        }
+
+        private String getAddressByLocation() {
+            String urlString = String.format(queryURLString, latLng.latitude, latLng.longitude);
+            try {
+                String jsonStr = HttpUtil.get(urlString);
+                JSONArray results = new JSONObject(jsonStr).getJSONArray("results");
+                if (results.length() >= 1) {
+                    JSONObject jsonObject = results.getJSONObject(0);
+                    address = jsonObject.optString("formatted_address");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            return address;
+        }
+    }
+
+
+    private static class AddressToLatLng implements Callable<LatLng> {
+        private String queryURLString = DOMAIN + "?address=%s&sensor=false&language=zh_tw";
+        private String address;
+
+        AddressToLatLng(String address) {
+            this.address = address;
+        }
+
+        @Override
+        public LatLng call() {
+            LatLng latLng = null;
+            try {
+                latLng = getLocationByAddress(URLEncoder.encode(address, "UTF-8"));
+            } catch (UnsupportedEncodingException ex) {
+
+            }
+            return latLng;
+        }
+
+        private LatLng getLocationByAddress(String address) {
+            String urlString = String.format(queryURLString, address);
+            LatLng latLng = null;
+            try {
+                String jsonStr = HttpUtil.get(urlString);
+                JSONArray results = new JSONObject(jsonStr).getJSONArray("results");
+                System.out.println("results.length():" + results.length());
+                if (results.length() >= 1) {
+                    JSONObject jsonObject = results.getJSONObject(0);
+                    JSONObject laln = jsonObject.getJSONObject("geometry").getJSONObject("location");
+                    latLng = new LatLng(Double.parseDouble(laln.getString("lat")), Double.parseDouble(laln.getString("lng")));
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return latLng;
+        }
     }
 }
--- googlemaps_demo_camera/src/main/java/com/mian/googlemaps_demo_camera/MainActivity.java
+++ googlemaps_demo_camera/src/main/java/com/mian/googlemaps_demo_camera/MainActivity.java
@@ -1,15 +1,18 @@
 package com.mian.googlemaps_demo_camera;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -36,16 +39,17 @@ public class MainActivity extends AppCompatActivity {
         uiSettings.setTiltGesturesEnabled(true);//傾斜手勢
         uiSettings.setRotateGesturesEnabled(true);//旋轉手勢
 
-
+        //設定點擊地圖的某一點時的監聽介面
         myMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
+                //根據latlng取得該經緯度所對應的地址或地標
                 String address = Helper.getAddressByLatLng(latLng);
                 if (address == null) {
                     Toast.makeText(context, "Not found !", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(context, address, Toast.LENGTH_SHORT).show();
-                    playAnimateCamera(latLng, 3000);
+                    playAnimateCamera(latLng, 1000);
                 }
             }
 
@@ -65,19 +69,37 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case 1:
                 LatLng latLng = new LatLng(25.047924, 121.517081);
-                playAnimateCamera(latLng, 10000);
+                playAnimateCamera(latLng, 1000);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void playAnimateCamera(LatLng latLng, int i) {
+    /**
+     * 在地圖上顯示地圖相機鏡頭動畫效果
+     *
+     * @param latLng     包裝著座標的LatLng物件
+     * @param durationMs 動畫持續的時間(毫秒)
+     */
+    private void playAnimateCamera(LatLng latLng, int durationMs) {
+        //設定地圖相機鏡頭的位置參數
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)//座標的位置
+                .zoom(17.0f)//縮放的大小，要 >= 17 才會顯示建築物
+                .bearing(300)//旋轉的角度
+                .tilt(67)//傾斜的角度
+                .build();
 
+        //取得CameraUpdate物件，定義地圖相機鏡頭的移動
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        //地圖相關鏡頭動畫行程設定
+        myMap.animateCamera(cameraUpdate, durationMs, null);
     }
 
 
     /**
-     * 取得GoogleMap物件
+     * 取得GoogleMap物件，確保GoogleMap物件不為null
      */
     private void setUpMapIfNeeded() {
         if (myMap == null) {
